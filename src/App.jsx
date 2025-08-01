347e503a058a3f954e4b186834326dd04347df37
Juan Santos <juan.moreira62@outlook.com>
2025-08-01 17:51:30 -0300

    feat: setting darkMode function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from './components/Hero'
 
 
 
 const App = () => {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkMode])
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
+
   return (
-    <div>
-      <Hero />      
+    <div className='min-h-screen bg-neutral-100 dark:bg-gray-950 relative transition-colors duration-300'>
+      <button onClick={toggleDarkMode} className='fixed top-3 lg:top-4 right-3 lg:right-4 w-9 h-9 lg:w-10 lg:h-10 flex justify-center items-center rounded-full bg-amber-500 text-neutral-950 shadow-lg hover:bg-amber-600 transition-colors'>
+        <i className={`bx bx-${darkMode ? 'sun' : 'moon'} text-lg lg:text-xl`}></i>
+      </button>
+      <Hero />
     </div>
   )
 }
